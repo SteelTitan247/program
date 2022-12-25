3d83f17381da81b698065ac1320114867d3d7bec
Shreyansh Sharma <64141720+SteelTitan247@users.noreply.github.com>
2022-12-25 17:43:16 +0530

    A program which convert given feet value to inches
    This is a C++ program which converts feet value to inches, with exact input type to output type as provided.

---
+++ program/convert-inches-to-feet/ConvertInchesToFeet.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    double inches;
+    //cout<<"Enter the value in inches: ";
+    cin>>inches;
+    double feet=inches/12;
+    //cout<<"Your value in feet is: "<<feet;
+    cout<<feet;
+}
